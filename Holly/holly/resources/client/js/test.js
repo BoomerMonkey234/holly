fa4ca58cd23ccff4042f46fed32e6331e4d07ef6
Alex James <56876514+BoomerMonkey234@users.noreply.github.com>
2021-02-05 18:48:53 +0000

    Delete Holly directory

--- Holly/holly/resources/client/js/test.js
+++
@@ -1,7 +0,0 @@
-function pageload(){
-    //debugger;
-    let Userdiv = document.getElementById("uNameDetail");
-    let Email = Cookies.get("Email");
-    Userdiv.innerHTML = "You are signed in as " + Email;
-
-}
